c68c124af46375985be6c73269e572699e9c4050
sergssnorth <sergssnorth@gmail.com>
2023-11-27 23:13:36 +0300

    add page

--- logopedcentre_frontend/src/router/index.js
+++ logopedcentre_frontend/src/router/index.js
@@ -1,5 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/Home.vue'
+import SignUp from '../views/SignUp.vue'
+import LogIn from '../views/LogIn.vue'
+import Specialists from '../views/Specialists.vue'
+import Record from '../views/Search.vue'
+import MyAccount from '../views/MyAccount.vue'
+import store from '../store'
 
 const routes = [
   {
@@ -8,14 +14,12 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
 ]
+
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
