371fb19bd4837ce2af33b869884ab1db6b45612c
sergss <sergssnorth@mail.ru>
2023-11-27 20:33:20 +0300

    StrartVue

--- logopedcentre_frontend/src/App.vue
+++ logopedcentre_frontend/src/App.vue
@@ -1,30 +1,156 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="wrapper">
+    <nav class="navbar is-spaced is-size-5">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">
+          <img src="@/assets/logoped.jpg" width="112" height="">
+        </router-link>
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-targer="navbar-menu" @click="showMobileMenu =!showMobileMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <form method="get" action="/search">
+              <div class="field has-addons">
+                <div class="control">
+                  <input type="text" class="input" placeholder="Фамилия Имя Отчество" name="query">
+                </div>
+
+                <div class="control">
+                  <button class="button is-info">
+                      <span class="icon">
+                        <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+                      </span>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="navbar-end">
+          <router-link to="/record" class="navbar-item">Запись</router-link>
+          <router-link to="/specialists" class="navbar-item">Специалисты</router-link>
+          <router-link to="/about-us" class="navbar-item">О нас</router-link>
+
+          <div class="navbar-item">
+            <div class="buttons">
+              
+              <template v-if="$store.state.isAuthenticated">
+                <router-link to="/my-account" class="button is-light">Мой аккаунт</router-link>
+                <router-link to="/cart" class="button is-success">
+                  <span>Мои записи ({{ cartTotalLength }})</span>
+                </router-link>
+              </template>
+
+              <template v-else>
+                <router-link to="/log-in" class="button is-outlined is-info">
+                  <!-- <span class="icon">
+                    <font-awesome-icon icon="fa-solid fa-house" />
+                  </span> -->
+                  <span>Вход</span>
+                </router-link>
+              </template>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    
+    <div class="is-loading-bar has-text-centered" v-bind:class="{'is-loading': $store.state.isLoading}">
+      <div class="lds-dual-ring"></div>
+    </div>
+
+    <section class="section is-spaced">
+      <router-view/>
+    </section>
+    <footer class="footer has-text-centered is-flex-align-items-flex-end mt-auto">
+      <p class="has-text-centered">Copyright (c) 2023</p>
+    </footer>
+  </div>
 </template>
 
+<script>
+import axios from 'axios'
+
+
+export default{
+  data() {
+    return {
+      showMobileMenu: false,
+      cart: {
+        items: []
+      }
+    }
+  },
+  beforeCreate() {
+    this.$store.commit('initializeStore')
+    
+    const token = this.$store.state.token
+
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = "Token " + token
+    } else {
+        axios.defaults.headers.common['Authorization'] = ""
+    }
+  },
+  mounted() {
+    this.cart = this.$store.state.cart
+  },
+  computed: {
+    cartTotalLength() {
+      let totalLength = 0
+
+      for (let i = 0; i < this.cart.items.length; i++) {
+        totalLength += this.cart.items[i].quantity
+      }
+
+      return totalLength
+      
+    }
+  }
+}
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import '../node_modules/bulma';
+.lds-dual-ring {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #ccc;
+  border-color: #ccc transparent #ccc transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 
-nav {
-  padding: 30px;
+.is-loading-bar {
+  height: 0;
+  overflow: hidden;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  &.is-loading {
+    height: 80px;
   }
 }
 </style>
--- logopedcentre_frontend/src/main.js
+++ logopedcentre_frontend/src/main.js
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
-createApp(App).use(store).use(router).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import { faUserSecret, 
+        faMagnifyingGlass,
+        faHouse 
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserSecret, faMagnifyingGlass, faHouse)
+
+createApp(App).use(store).use(router, axios).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
